39d214212742079a0bdea136f51f0f7bbca5e077
Akshay Nair <phenax5@gmail.com>
2016-12-14 21:17:49 +0530

    Refactored tests

--- tests/app-test.spec.js
+++ tests/app-test.spec.js
@@ -6,60 +6,62 @@ const { expect }= require('chai');
 const app= require('../app');
 
 
-// Make sure everything is ready to go
-describe('App Configuration', () => {
+describe('BlogApp', () => {
+
+	// Make sure everything is ready to go
+	describe('App Configuration', () => {
+
+		it('should have port', () => {
+
+			expect(app.port).to.exist;
+
+			expect(typeof app.port).to.eql('number');
+		});
 
 
-	it('should have port', () => {
+		it('should have atleast one route', () => {
 
-		expect(app.port).to.exist;
+			expect(app._routes).to.not.be.empty;
+		});
 
-		expect(typeof app.port).to.eql('number');
+
+		it('should have a custom 404 error handler', () => {
+
+			expect(app.error).to.exist;
+
+			expect(app.error).to.be.instanceof(Function);
+
+			expect(app.error).to.not.eql(app.error404);
+		});
 	});
 
 
-	it('should have atleast one route', () => {
+	// All routing functionality works
+	describe('App Router', () => {
 
-		expect(app._routes).to.not.be.empty;
-	});
+		const darkSideCtrlr= (req, res) => {};
 
+		const darkSideUrl= '/darth-vader-will-rise';
 
-	it('should have a custom 404 error handler', () => {
+		beforeEach(() => {
+			app.addRoute(new RegExp(`^${darkSideUrl}$`), darkSideCtrlr);
+		});
 
-		expect(app.error).to.exist;
-
-		expect(app.error).to.be.instanceof(Function);
-
-		expect(app.error).to.not.eql(app.error404);
-	});
-});
-
-
-// All routing functionality works
-describe('App Router', () => {
-
-	const darkSideCtrlr= (req, res) => {};
-
-	const darkSideUrl= '/darth-vader-will-rise';
-
-	beforeEach(() => {
-		app.addRoute(new RegExp(`^${darkSideUrl}$`), darkSideCtrlr);
-	});
-
-	afterEach(() => {
-		app.removeRoute({ controller: darkSideCtrlr });
-	});
+		afterEach(() => {
+			app.removeRoute({ controller: darkSideCtrlr });
+		});
 
 
-	it('should be able to match routes', () => {
+		it('should be able to match routes', () => {
 
-		const matches= app.getMatchingRoute(darkSideUrl);
+			const matches= app.getMatchingRoute(darkSideUrl);
 
-		expect(matches).to.not.be.empty;
+			expect(matches).to.not.be.empty;
 
-		const controller= matches[0].get('controller');
+			const controller= matches[0].get('controller');
 
-		expect(controller).to.eql(darkSideCtrlr);
+			expect(controller).to.eql(darkSideCtrlr);
+		});
 	});
 });
 
--- tests/blog-test.spec.js
+++ tests/blog-test.spec.js
@@ -8,75 +8,79 @@ const { expect, assert }= require('chai');
 const blogs= require('../lib/Blogs');
 
 
-describe('Blogs configuration and utils', () => {
+describe('Blog Creator', () => {
 
-	it('should convert the title to file-friendly string', () => {
+	describe('Blogs configuration and utils', () => {
 
-		expect(blogs.filterTitle('Title Gets Converted')).to.eql('title-gets-converted');
+		it('should convert the title to file-friendly string', () => {
 
-		expect(blogs.filterTitle('SD"F\'D\/G/D')).to.eql('sdfd_g_d');
-	});
-});
+			expect(blogs.filterTitle('Title Gets Converted')).to.eql('title-gets-converted');
 
-
-describe('Blogs API', () => {
-
-	let title;
-	const blogContent= 'sakfjsidghkdjfxghkjdfhjkgdhjkfghj';
-
-	beforeEach((done) => {
-
-		blogs.addBlog('Awesome Blog', blogContent, (err, _title) => {
-
-			if(err) {
-				done(err);
-				return;
-			}
-
-			title= _title;
-			done();
+			expect(blogs.filterTitle('SD"F\'D\/G/D')).to.eql('sdfd_g_d');
 		});
 	});
 
-	afterEach((done) => {
 
-		blogs
-			.deleteAll()
-			.then( _ => done());
-	});
+	describe('Blogs API', () => {
 
+		let title;
+		const blogContent= 'sakfjsidghkdjfxghkjdfhjkgdhjkfghj';
 
-	it('should fetch the correct blog', (done) => {
+		beforeEach((done) => {
 
-		blogs
-			.getBlog(title)
-			.then( body => {
+			blogs.addBlog('Awesome Blog', blogContent, (err, _title) => {
 
-				expect(body).to.eql(blogContent);
+				if(err) {
+					done(err);
+					return;
+				}
+
+				title= _title;
 				done();
-			})
-			.catch( e => done(e) );
+			});
+		});
+
+		afterEach((done) => {
+
+			blogs
+				.deleteAll()
+				.then( _ => done());
+		});
+
+
+		it('should fetch the correct blog', (done) => {
+
+			blogs
+				.getBlog(title)
+				.then( body => {
+
+					expect(body).to.eql(blogContent);
+					done();
+				})
+				.catch( e => done(e) );
+		});
+
+
+		it('should update contents', done => {
+
+			const newContents= 'this is an awesome post\n awesomely updated';
+
+			blogs
+				.updateBlog(title, newContents)
+				.then( _ => {
+
+					blogs
+						.getBlog(title)
+						.then( body => {
+
+							expect(body).to.not.eql(blogContent);
+							done();
+						})
+						.catch( e => done(e) );
+				})
+				.catch( e => done(e) );
+		});
 	});
 
-
-	it('should update contents', done => {
-
-		const newContents= 'this is an awesome post\n awesomely updated';
-
-		blogs
-			.updateBlog(title, newContents)
-			.then( _ => {
-
-				blogs
-					.getBlog(title)
-					.then( body => {
-
-						expect(body).to.not.eql(blogContent);
-						done();
-					})
-					.catch( e => done(e) );
-			})
-			.catch( e => done(e) );
-	});
 });
 
