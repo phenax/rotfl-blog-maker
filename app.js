9cec97d2f78e1c7802e0b079a263422b5b44848c
Akshay Nair <phenax5@gmail.com>
2016-12-14 21:35:41 +0530

    docs and refactoring

--- app.js
+++ app.js
@@ -6,23 +6,27 @@ class App extends NodeApp {
 	constructor(config) {
 		super(config);
 
+		// Routes config
 		this.onError(this.errorHandler)
 			.addRoute(/^\/$/, this.indexController)
 			.addRoute(/^\/admin(\/(.*))?$/, this.adminController);
 	}
 
+	// controller for the index route
 	indexController() {
 
 		this.renderFile('./index.html')
 			.catch(e => this.triggerError(500, e));
 	}
 
+	// controller for the /admin route
 	adminController() {
 
 		this.render('AdminPanel')
 			.catch(e => this.triggerError(500, e));
 	}
 
+	// error handling route
 	errorHandler(req, res) {
 		res.writeHead(404);
 		res.end('Error happens dude. Calm down.');
@@ -31,5 +35,7 @@ class App extends NodeApp {
 
 
 module.exports= new App({
+
+	// Runnin it on port 8080
 	port: 8080
 });
